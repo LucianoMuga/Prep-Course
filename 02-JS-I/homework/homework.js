b6c70746de3c1630efc7c7df7c78c065b5ed7a0f
LucianoMuga <lucho.muga2@gmail.com>
2022-01-29 13:31:08 -0300

    vuelta commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -81,7 +81,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  return if (num > 50) 
+  return(num)
 }
 
 function obtenerResto(x, y) {
