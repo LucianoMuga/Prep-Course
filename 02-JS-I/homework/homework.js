0735ba370ea363f00b45e382aea6f4280307f4bc
LucianoMuga <lucho.muga2@gmail.com>
2022-01-31 16:35:21 -0300

    perceccionando

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,7 +74,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num < 90
+  return num <= 90
 }
 
 function mayorQueCincuenta(num) {
