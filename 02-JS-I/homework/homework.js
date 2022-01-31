846b9cee185b90386a14daa36382f81d35d18068
LucianoMuga <lucho.muga2@gmail.com>
2022-01-31 17:34:37 -0300

    jaja

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,9 +74,10 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num < 90)
+ return if(num < 90)
 
 }
+
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
