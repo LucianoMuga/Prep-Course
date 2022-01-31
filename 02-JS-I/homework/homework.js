121df94ac013a5bed082db19f503fe0806b25083
LucianoMuga <lucho.muga2@gmail.com>
2022-01-31 18:54:22 -0300

    redondeos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -127,19 +127,19 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  return(num)
+  return Math.round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return(num)
+  return Math.ceil(num)
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return Math.random
+  return Math.random(0)
 }
 
 function esPositivo(numero) {
