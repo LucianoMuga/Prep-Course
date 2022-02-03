9cbc7a7a3de287aebef6aa4d94706705f5806aeb
LucianoMuga <lucho.muga2@gmail.com>
2022-02-03 12:32:34 -0300

    ananneneshe

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,7 +175,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinados = nombres + 'Henry' + apellido;
+  var combinados = nombres + 'Henry' + apellido
   return combinados;
 }
 
@@ -211,7 +211,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return 1 * 1.20
+  return euro * 1.20
 }
 
 
