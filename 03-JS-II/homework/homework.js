943c87cdf2f58827bd62e194c9210a113e447680
LucianoMuga <lucho.muga2@gmail.com>
2022-02-03 20:03:58 -0300

    lsadoaske

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -112,7 +112,7 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
 if(numero % 3 && 5 === 0){
-return (fizzbuzz)
+return "fizzbuzz"
 } else if(numero % 3 === 0){
   return "fizz"
 }else if(numero % 5 === 0){
