f765f32e134beb788a9b714954d421cb9a4cfd39
LucianoMuga <lucho.muga2@gmail.com>
2022-02-03 19:42:46 -0300

    jasidjas

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -78,9 +78,9 @@ function esDiezOCinco(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (numero === 10 || numero === 5){
-  return "true"
+  return true;
 }  else{
-  return "false"
+  return false;
 }
 }
 
@@ -88,7 +88,9 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  return numero < 50 && numero > 20
+  if(numero < 50 && numero > 20) return true;
+  return false
+
 }
 
 function esEntero(numero) {
