1395512bb730ffff41ee90bc16bb63c1d20db754
LucianoMuga <lucho.muga2@gmail.com>
2022-02-03 12:24:17 -0300

    apunto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,7 +175,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinados = nombres + 'Luciano' + apellido
+  var combinados = nombres + 'Henry' + apellido;
   return combinados;
 }
 
@@ -189,7 +189,7 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return alto * ancho
+  return alto * ancho;
 }
 
 
@@ -204,7 +204,7 @@ function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
  return(base * altura)/2
- 
+
 }
 
 function deEuroAdolar(euro){
@@ -221,8 +221,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length > 1) 
+  if(letra.length > 1){
   return "Dato incorrecto"
+  }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u" ){
   return "Es vocal"
    } else { 
