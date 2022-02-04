5360504d86d8f4bc703615c133373cf97b7a362a
LucianoMuga <lucho.muga2@gmail.com>
2022-02-04 16:33:01 -0300

    opop

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -131,10 +131,10 @@ function operadoresLogicos(num1, num2, num3) {
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if(num1 < 0 || num2 < 0 || num3 < 0){
     return "Hay negativos"
+}  else if(num1 === 0 && num2 === 0 && num3 === 0){
+    return "error"
 }  else if(num1 > 0 && num1 > num2 && num1 > num3){
   return "Numero 1 es mayo y positivo"
-} else if(num1 === 0 && num2 === 0 && num3 === 0){
-    return "error"
 }else if(num3 > num1 && num3 > num2){
 return num3 + 1;
 }else{
