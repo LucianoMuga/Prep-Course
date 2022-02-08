8502c54d930bf12ebf81f7d08f3f3f1f20c97423
LucianoMuga <lucho.muga2@gmail.com>
2022-02-08 12:46:38 -0300

    sd

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -35,7 +35,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  
+  array.push("elemento");
+  return array
 }
 
 
