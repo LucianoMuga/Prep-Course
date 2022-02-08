e6d37f29a7abbb7c07349ddcf39e6be253037256
LucianoMuga <lucho.muga2@gmail.com>
2022-02-08 14:31:33 -0300

    asldjka

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,11 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-var newarray = [];
+var nuevoArray = [];
 for(var i = 0; i <= array.lenght; i++){
-  newarray[i] = array[i] + 1;
+  nuevoArray[i] = array[i] + 1;
 }
-return array
+return nuevoArray;
 
 }
 
