d9c2b41efe92831828bc5c060ab0c80cc26686e9
LucianoMuga <lucho.muga2@gmail.com>
2022-02-08 14:42:03 -0300

    das

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,7 +10,8 @@ function devolverPrimerElemento(array) {
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-  return array[array.lenght -1]
+  return array[array.length - 1];
+  
 }
 
 
