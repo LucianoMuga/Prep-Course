f22308d9f6ee434ad20f633acf5a4d45fa1bd1f5
LucianoMuga <lucho.muga2@gmail.com>
2022-02-08 22:19:43 -0300

    sdas

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -40,8 +40,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array.push[elemento]
-  return array;
+  array.push[elemento];
+  return array
 }
 
 
@@ -50,9 +50,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  array.unshift(elemento);
+  array.unshift(elemento)
   return array;
-
+  
 }
 
 
