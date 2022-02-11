11e14367c876f0255fbaf03540d632638716aedf
LucianoMuga <lucho.muga2@gmail.com>
2022-02-11 18:11:51 -0300

    terinado js II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -157,8 +157,9 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   if(numero === 0 || numero === 1) return false
-  for (var Q=2; Q < numero; i++){
-  if (numero % Q === 0) return false
+  if(numero === 2) return true
+  for (var i = 2; i < numero; i++){
+  if (numero % i === 0) return false
   }
   return true;
 
@@ -170,9 +171,9 @@ function esVerdadero(valor){
   //Escribe tu código aquí
   if(valor === true){
     return "Soy verdadero"
-  } else if (varlor === false){
+  }
  return "Soy falso"
-  } 
+   
 }
 
 function tablaDelSeis(){
@@ -180,8 +181,8 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí 
   var tabladel6 = [];
-  for ( var i = 0; i < 11; i++){
-  tabladel6.push(6 * i)
+  for ( var i = 0; i <= 10; i++){
+  tabladel6.push(6 * [i])
 }
   return tabladel6
 }
