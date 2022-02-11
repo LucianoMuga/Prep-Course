582504494b42aeb62180910bc1b3e9379277e5a2
LucianoMuga <lucho.muga2@gmail.com>
2022-02-11 18:49:52 -0300

    terminado JS I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -67,7 +67,12 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
- srt1.length === str2.lenght
+ if(str1.length === str2.length){
+   return true
+ } else {
+   return false
+ }
+ 
 }
 
 function menosQueNoventa(num) {
@@ -168,22 +173,22 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:(
-  return '$(str)!'
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinados = nombres + '' + apellido;
-  return combinados;
+  return (nombre + ' ' + apellido);
+  
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre + '!';
+  return 'Hola' + nombre + '!'
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
